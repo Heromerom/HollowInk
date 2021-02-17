bb1a0d8238fb3338223c7daf6e797a8e23d40dbb
HollowInk <49551386+Heromerom@users.noreply.github.com>
2021-02-16 22:34:29 -0300

    Create Discordhook.js

---
+++ Discordhook.js
@@ -0,0 +1,60 @@
+{
+  "content": null,
+  "embeds": [
+    {
+      "title": ":wave:  __WELCOME TO THE SERVER__",
+      "description": "Hello there! The name is <@494626084285972491> - also known as Nulgath, the OverSoul Arch Fiend. I'm developing Akumi Wars, an RPG in Akumi Universe game. Feel free to roam around my server.",
+      "color": 65535,
+      "image": {
+        "url": "https://media.discordapp.net/attachments/572193877549907978/799512360758542396/Title_Screen_DOT_COM_1.png"
+      }
+    },
+    {
+      "title": ":orange_book: __RULES__",
+      "description": "• Please be respectful of others.\n• Please don’t use hateful or derogatory language.\n• Doxxing isn't allowed.\n• Do **NOT** post NSFW on public channels. Use the <#571471441892081674> channel instead.",
+      "color": 14745344
+    },
+    {
+      "title": ":shield: __ROLES__",
+      "color": 65535,
+      "fields": [
+        {
+          "name": "STAFF",
+          "value": "<@&571752772375150633> - _role given to the master Miltonius._\n<@&571731373141000232> - _moderators for this server._\n<@&760617834337402920> - _manage the server and its bots._\n<@&571753935543730176> - _server bots._"
+        },
+        {
+          "name": "SUPPORT",
+          "value": "Support Miltonius through Patreon and get exclusive roles. Those are displayed separately from other roles.\n<@&572661796674994176> - _the role was given to people who support Miltonius on Patreon, Twitch, or Subscribstar._\n<@&759503385144983603> - _the role was given to people who boosted this server._"
+        },
+        {
+          "name": "MILTONIUS ARTS",
+          "value": "_Roles are given according to the perk bought at Patreon.com/MiltoniusArts._\n<@&764715367095337011> - <@&764715373131595837> - <@&764715376755343371> - <@&764715379502219304>",
+          "inline": true
+        },
+        {
+          "name": "AKUMI WARS",
+          "value": "_Roles are given according to the perk bought at Patreon.com/Akumi._\n<@&765311520962904125> - <@&765311461077286913> - <@&766197814253584394>",
+          "inline": true
+        },
+        {
+          "name": "LEVELING ROLES",
+          "value": "Level up while chatting! We have a leveling system that assigns roles according to your level here in our Discord.\n\n__Level 1__ - <@&571769668742938675>\n__Level 5__ - <@&571769733880348683>\n__Level 10__ - <@&571769774627880960>\n__Level 15__ - <@&571769847692918787>\n__Level 25__ - <@&571769887899385856>\n__Level 45__ - <@&571769982481072129>\n__Level 55__ - <@&571807084014862356>\n__Level 65__ - <@&571770038759981071>\n__Level 75__ - <@&571770122675552257>\n__Level 85__ - <@&571770152094269465>\n__Level 100__ - <@&571770174340988948>\n\nType `!rank` to see your level and your rank. `!levels` will show the leaderboard."
+        }
+      ]
+    },
+    {
+      "title": ":globe_with_meridians:  __SOCIAL NETWORKS__",
+      "description": "Do you want to **fund me** at Patreon or **follow** my ALL social networks? Then check the links bellow.\n\n__Akumi Wars__ - https://AkumiWars.com\n__Patreon__ - https://www.patreon.com/miltoniusarts\n__Discord__ - https://discord.gg/UqGuzcB\n__Twitter__ - https://twitter.com/MiltoniusArts\n__Youtube__ - https://www.youtube.com/channel/UCx4R27J7wUl4ToKaV5c42VQ\n\nGot questions? Check out our **Frequently Asked Questions** (<#811406159030124585>).",
+      "color": 65351,
+      "footer": {
+        "text": "Writen by Heromerom.",
+        "icon_url": "https://i.imgur.com/bkoqi19.png"
+      },
+      "image": {
+        "url": "https://dim.mcusercontent.com/cs/5f152cf29abff065005954291/images/d29dc34a-484c-4288-b36c-8195d0d3606e.png?w=528&dpr=1"
+      }
+    }
+  ],
+  "username": "Miltonius",
+  "avatar_url": "https://images-ext-1.discordapp.net/external/gVUqzzgp0ufdA90VxgNsoEjVrZZM1uUeUD4_OmUq8lk/%3Fsize%3D256/https/cdn.discordapp.com/avatars/494626084285972491/b08d89801cca0b82400a4246d4573b34.png"
+}
